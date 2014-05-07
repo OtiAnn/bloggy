5e53c5170cc56542fef863a71a498e86c94ad81c
OtiAnn <anniebh30@gmail.com>
2014-05-07 17:46:13 +0800

    刪掉comments,修改css樣式

--- app/views/posts/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @post, :id, :title, :body, :picture, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :comments
+  get 'home/index'
 
   resources :categories
 
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'posts#index'
+  root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
