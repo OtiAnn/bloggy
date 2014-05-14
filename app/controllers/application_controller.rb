6852df07ee3f175412cd095e55bbb380bb7e80f8
OtiAnn <anniebh30@gmail.com>
2014-05-14 19:46:56 +0800

    讓登入頁的username可被存下來

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :user_name
+  end
+
   protect_from_forgery with: :exception
   helper_method :all_categories
   helper_method :all_posts
@@ -17,4 +25,5 @@ class ApplicationController < ActionController::Base
   def all_comments
     @comments = Comment.all
   end
+
 end
