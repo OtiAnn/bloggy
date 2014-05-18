c5e76e855ad1a6058e7e2767e2755b5a0171de62
OtiAnn <anniebh30@gmail.com>
2014-05-18 23:33:27 +0800

    加入jQuery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,4 @@
+
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
@@ -14,3 +15,17 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$().ready(function() {
+
+	$("#toggle-sidebar").click(function() {
+		$('.sidebar').toggleClass('js-slide-left')
+		// if($('.sidebar').hasClass('js-slide-left')==true){
+		// 	$('.sidebar').removeClass('js-slide-left');
+		// }else{
+		// 	$('.sidebar').addClass('js-slide-left');
+		// }
+	});
+
+
+});
