46b02b7e285e0ebb653f381fbb3b7f5fac3d323a
OtiAnn <anniebh30@gmail.com>
2014-06-11 21:51:19 +0800

    修復語系

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,7 @@ module Bloggy
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.default_locale = "zh-TW"
+    I18n.load_path += Dir[Rails.root.join('lib', 'locale', '*.{rb,yml}')]
+    config.i18n.default_locale = 'zh-TW'
   end
 end
